dfc6f0ad30d9358d0ca76a8868d2d3c41a3cabd7
shenyuan <775486362@qq.com>
2023-06-09 16:53:05 +0800

    feat: 增加effect的scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,7 +1,7 @@
 class ReactiveEffect {
   private _fn: any;
 
-  constructor(fn) {
+  constructor(fn, public scheduler) {
     this._fn = fn;
   }
 
@@ -32,13 +32,17 @@ export function trigger(target, key) {
   const depsMap = targetMap.get(target);
   const dep = depsMap.get(key);
   for(let effect of dep) {
-    effect.run();
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 };
 
 let activeEffect;
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn);
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options?.scheduler);
   _effect.run();
   return _effect.run.bind(_effect);
 }
